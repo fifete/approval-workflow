c8de6693556889f808109c742b11496b6ef180a0
fifete <danielafuentesdev@mgmail.com>
2024-10-28 19:12:45 -0500

    feat: add action button

--- src/app/home/components/requests/columns.tsx
+++ src/app/home/components/requests/columns.tsx
@@ -1,6 +1,9 @@
 "use client"
 
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuItem, DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu"
 import { ColumnDef } from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react"
+import { Button } from "~/app/_components/ui/button"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -42,4 +45,25 @@ export const columns: ColumnDef<Request>[] = [
       accessorKey: "status",
       header: "Status",
     },
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Approve</DropdownMenuItem>
+              <DropdownMenuItem>Reject</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )
+      },
+    }
   ]
